03aaee1c7a448586388f3639bf646015b7879ca2
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-04-17 13:50:05 +0200

    keysolo is completed

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,17 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+
+    document.addEventListener('keyup', (symbol) => {
+
+      if (symbol.key === this.currentSymbol.textContent) {
+        return this.success();
+      }
+
+      else {
+        return this.fail(); 
+      }
+    })  
   }
 
   success() {
