409c2bae9b002cd9f31c0e087a863e4835bb8c06
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-04-25 18:46:13 +0200

    tooltip complete

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,20 @@
+const tooltips = document.querySelectorAll('.has-tooltip');
+
+for (i = 0; i < tooltips.length; i++) {
+    tooltips.item(i).onclick = (event) => {
+        const tooltipActive = document.querySelector('.tooltip_active');
+        if (tooltipActive !== null) {
+            tooltipActive.remove();
+        }        
+        const element = event.target;
+        const tooltip = document.createElement('div');
+        tooltip.className = "tooltip tooltip_active";
+        tooltip.style.position = 'absolute';       
+        tooltip.style.top = `${element.getBoundingClientRect().top - 40 + window.scrollY}px`;
+        tooltip.style.left = `${element.getBoundingClientRect().left}px`;
+        tooltip.textContent = element.title;
+        element.insertAdjacentHTML('afterend', tooltip.outerHTML);       
+        return false;
+        }
+    }
+    
